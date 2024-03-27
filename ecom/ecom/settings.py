d1c3070adfeee58ae3e3588b3b4008244155efb9
CamilleusRex <camilleusrex@gmail.com>
2024-03-27 22:53:32 +0100

    added media root to settings.py

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -95,6 +96,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
