a4b0bf01ff54d54802239cddae89a0968d604eac
CamilleusRex <camilleusrex@gmail.com>
2024-03-27 22:54:00 +0100

    added nasic models to store/models.py

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name    
+
+
+class Customer(models.Model):
+    user_name = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.user_name}, {self.first_name} {self.last_name}'
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, max_digits=7, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=500, default='', bland=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return self.name   
+    
+    
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='', null=True, blank=True)
+    phone = models.CharField(max_length=15, default='', null=True, blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product  
