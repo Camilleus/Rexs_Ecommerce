a7bc2e0994aa0db694f8a3e90d09c8f745851b19
CamilleusRex <camilleusrex@gmail.com>
2024-03-27 23:03:53 +0100

    added models to admin panel

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Product, Order, Customer
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Customer)
