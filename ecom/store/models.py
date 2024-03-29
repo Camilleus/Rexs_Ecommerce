bf1fb6add757e768bf41af1f48c4d5608fc8f5e8
CamilleusRex <camilleusrex@gmail.com>
2024-03-29 07:34:39 +0100

    added plural form of categorys

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name    
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
 class Customer(models.Model):
