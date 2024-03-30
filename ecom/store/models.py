189b800779a326d35784b80d4a7e3eed3eece1f0
CamilleusRex <camilleusrex@gmail.com>
2024-03-30 16:27:57 +0100

    added sales feature

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -32,6 +32,12 @@ class Product(models.Model):
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
+    # Sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, max_digits=7, decimal_places=2)
+    # Bestseller Stuff
+    is_bestseller = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.name   
     
