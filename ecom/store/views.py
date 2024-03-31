23be3a3dc095a77ef7e61c1c5fce02de38b9bc2b
CamilleusRex <camilleusrex@gmail.com>
2024-03-31 21:08:10 +0200

    added basics of login logic

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Customer, Product, Order
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home_page(request):
@@ -14,3 +15,19 @@ def product_page(request):
 
 def about_page(request):
     return render(request, 'about.html', {})
+
+
+def signin_page(request):
+    messages.success(request, 'Signed in successfully')
+    return render(request, 'signin.html', {})
+
+
+def login_user_page(request):
+
+        return render(request, 'login.html', {})
+
+
+def logout_user_page(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
