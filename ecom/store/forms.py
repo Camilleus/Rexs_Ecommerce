e1ffb3c31795e556c067457ea6a0f6b653b689ee
CamilleusRex <camilleusrex@gmail.com>
2024-04-01 18:12:34 +0200

    added imports of User Django models

---
+++ ecom/store/forms.py
@@ -0,0 +1,3 @@
+from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django import forms
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Category, Customer, Product, Order
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django import forms
 
 
 def home_page(request):
