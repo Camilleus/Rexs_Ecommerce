de62c6d74125eed136d043138915f0ff687b7a71
CamilleusRex <camilleusrex@gmail.com>
2024-04-01 18:43:58 +0200

    Imported create_profile function

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -76,3 +76,17 @@ class Profile(models.Model):
 	
 	def __str__(self):
 		return self.user.username
+
+# Create Profile When New User Signs Up
+#@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+		# Have the user follow themselves
+		user_profile.follows.set([instance.profile.id])
+		user_profile.save()
+
+post_save.connect(create_profile, sender=User)
+
+
