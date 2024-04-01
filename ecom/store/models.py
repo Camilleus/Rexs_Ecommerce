0e56391f2b85e0dd37ed6206d2c749e881c7a236
CamilleusRex <camilleusrex@gmail.com>
2024-04-01 18:44:12 +0200

    imported Meep Model

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -90,3 +90,26 @@ def create_profile(sender, instance, created, **kwargs):
 post_save.connect(create_profile, sender=User)
 
 
+# create meep model
+class Meep(models.Model):
+	user = models.ForeignKey(
+		User, related_name="meeps", 
+		on_delete=models.DO_NOTHING
+		)
+	body = models.CharField(max_length=200)
+	created_at = models.DateTimeField(auto_now_add=True)
+	likes = models.ManyToManyField(User, related_name="meep_like", blank=True)
+
+
+	# Keep track or count of likes
+	def number_of_likes(self):
+		return self.likes.count()
+
+
+
+	def __str__(self):
+		return(
+			f"{self.user} "
+			f"({self.created_at:%Y-%m-%d %H:%M}): "
+			f"{self.body}..."
+			)
