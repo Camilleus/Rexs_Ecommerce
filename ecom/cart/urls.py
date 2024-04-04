efe5471f031dfd8ecfaddb9c0b0ad6b1123997ec
CamilleusRex <camilleusrex@gmail.com>
2024-04-04 13:12:00 +0200

    fixed cart urls

--- ecom/cart/urls.py
+++ ecom/cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.cart_page, name='cart_page'),
-    path('add', views.cart_add_page, name='cart_add_page'),
-    path('delete', views.cart_delete_page, name='cart_delete_page'),
-    path('update', views.cart_update_page, name='cart_update_page')
+    path('add/', views.cart_add_page, name='cart_add_page'),
+    path('delete/', views.cart_delete_page, name='cart_delete_page'),
+    path('update/', views.cart_update_page, name='cart_update_page')
 ]
