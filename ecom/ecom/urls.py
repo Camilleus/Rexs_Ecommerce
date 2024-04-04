b955dddded1abf61c0cfa533bd029abe529a509a
CamilleusRex <camilleusrex@gmail.com>
2024-04-04 12:46:22 +0200

    Update urls.py

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -7,5 +7,5 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('', include('cart.urls')),
+    path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
