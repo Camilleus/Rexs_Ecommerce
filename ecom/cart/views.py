e3d0fea9d41fab8eaf6a6e8f16cb8e96b888a75b
CamilleusRex <camilleusrex@gmail.com>
2024-04-06 14:01:20 +0200

    expanded cart_add view

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -19,8 +19,9 @@ def cart_add_page(request):
         product_id = int(request.POST.get('product_id'))
         product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product, qty=product_qty)
-        return JsonResponse({'status': 'success'})
+        cart.add(product=product, qty=product_qty) 
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
 
 def cart_update_page(request): 
     pass
