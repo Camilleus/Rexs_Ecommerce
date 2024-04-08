da261bceb6a09e08a8d0a369fff5d14da199e17c
CamilleusRex <camilleusrex@gmail.com>
2024-04-08 22:14:44 +0200

    added some more functions to the cart class

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -37,3 +37,17 @@ class Cart():
         return products
         
         
+    def get_product_total(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
