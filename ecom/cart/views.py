1e73f59947def224568faa95d0d01b8db2077fd0
CamilleusRex <camilleusrex@gmail.com>
2024-04-12 20:18:20 +0200

    added logic to cart_update_page

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-
+from django.urls import reverse
 
 from .cart import Cart
 from store.models import Category, Product
@@ -16,13 +16,14 @@ def cart_page(request):
     quantities = cart.get_quants()
     return render(request, 'cart_summary.html', {'products': products, 'categories': categories, 'cart_products': cart_products, 'quantities': quantities})
 
+
 def cart_add_page(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_qty = int(request.POST.get('product_qty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product, quantity=product_qty) 
+        cart.add(product=product, quantity=product_qty)
         cart_count = len(cart)
         response_data = {
             'product_name': product.name,
@@ -30,9 +31,21 @@ def cart_add_page(request):
         }
         return JsonResponse(response_data)
 
-def cart_update_page(request): 
-    pass
+
+def cart_update_page(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.update(product=product, quantity=product_qty)
+        cart_count = len(cart)
+        response_data = {
+            'product_name': product.name,
+            'cart_count': cart_count
+        }
+        return JsonResponse(response_data)
+
 
 def cart_delete_page(request):
     pass
-
